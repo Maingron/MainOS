0ef00cdfe66aeca1c98902607df9e7e2e4684c0b
Maingron <maingron@gmail.com>
2019-12-19 11:11:13 +0100

    Add key listener to helper.js - You can now close windows by pressing shift + space + w

--- helper.js
+++ helper.js
@@ -1,73 +1,129 @@
-// document.write("<link rel='stylesheet' type='text/css' href='../../helper.css'>");
-// document.write("<link rel='stylesheet' type='text/css' href='../../../helper.css'>");
+	document.documentElement.style.setProperty("--themecolor", window.parent.setting.themecolor);
+	document.documentElement.style.setProperty("--font", window.parent.setting.font);
+	document.documentElement.style.setProperty("--hovercolor", window.parent.setting.hovercolor);
+	document.documentElement.style.setProperty("--hovercolornontransparent", window.parent.setting.hovercolornontransparent);
 
-  
-	document.documentElement.style.setProperty("--themecolor",window.parent.setting.themecolor);
-    document.documentElement.style.setProperty("--font",window.parent.setting.font);
-    document.documentElement.style.setProperty("--hovercolor",window.parent.setting.hovercolor);
-    document.documentElement.style.setProperty("--hovercolornontransparent",window.parent.setting.hovercolornontransparent);
-    // document.documentElement.style.setProperty("--darkmodeblack","#000");
-    // document.documentElement.style.setProperty("--darkmodeblack2","#000");
-    // document.documentElement.style.setProperty("--darkmodeblack3","#000");
+var ismainos;
 
-    if(parent.setting.notsodarkmode == 1) {
-      document.documentElement.style.setProperty("--black","#151515");
-      document.documentElement.style.setProperty("--black2","#444");
-      document.documentElement.style.setProperty("--black3","#555");
-      document.documentElement.style.setProperty("--black4","#666");
-      document.documentElement.style.setProperty("--black5","#757575");
-    } 
+	if (parent.setting.notsodarkmode == 1) {
+	  document.documentElement.style.setProperty("--black", "#151515");
+	  document.documentElement.style.setProperty("--black2", "#444");
+	  document.documentElement.style.setProperty("--black3", "#555");
+	  document.documentElement.style.setProperty("--black4", "#666");
+	  document.documentElement.style.setProperty("--black5", "#757575");
+	}
 
-    var data = {};
-    data.system = {};
-    data.system.mouse = {};
-    
+	var data = {};
+	data.system = {};
+  data.system.mouse = {};
+  data.mypid = (window.parent.pid.length - 1);
 
-  document.addEventListener("contextmenu",function(event) {
-    console.log(event);
-    if(event.ctrlKey != true || event.shiftKey != true) {
-      event.preventDefault();
-      data.system.mouse.x = event.clientX;
-      data.system.mouse.y = event.clientY;
-      if(typeof contextMenu === "function") {
-        contextMenu(event);
-      }
+
+	document.addEventListener("contextmenu", function (event) {
+	  console.log(event);
+	  if (event.ctrlKey != true || event.shiftKey != true) {
+	    event.preventDefault();
+	    data.system.mouse.x = event.clientX;
+	    data.system.mouse.y = event.clientY;
+	    if (typeof contextMenu === "function") {
+	      contextMenu(event);
+	    }
+	  }
+
+	});
+
+	window.alert = alert = parent.notification;
+
+
+
+
+	function spawnContextMenu(content) {
+	  if (document.getElementsByClassName("contextMenu")[0]) {
+	    document.getElementsByClassName("contextMenu")[0].outerHTML = "";
+	  }
+	  var newelement = document.createElement("div");
+	  newelement.classList.add("contextMenu");
+	  newelement.style.left = data.system.mouse.x + "px";
+	  newelement.style.top = data.system.mouse.y + "px";
+	  document.body.append(newelement);
+
+	  for (var i = 0; content.length > i; i++) {
+	    newelement = document.getElementsByClassName("contextMenu")[0];
+	    if (content[i][0] == "<hr>") {
+	      var newcontextelement = document.createElement("hr");
+	    } else {
+	      var newcontextelement = document.createElement("a");
+	      newcontextelement.innerHTML = content[i][0];
+	      newcontextelement.href = "javascript:" + content[i][1];
+	      if (content[i][2] && content[i][2] == "disabled") {
+	        newcontextelement.removeAttribute("href")
+	      }
+
+	    }
+	    newelement.append(newcontextelement);
+	  }
+
+	  newelement.parentElement.addEventListener("click", function () {
+	    newelement.outerHTML = "";
+	  })
+	}
+
+	var key = [];
+
+	window.addEventListener("keydown", function (event) {
+
+
+	  key["current"] = event.key.toLowerCase();
+	  if (key["current"] == "control") {
+	    key["control"] = true;
+	  } else if (key["current"] == "shift") {
+	    key["shift"] = true;
+    } else if (key["current"] == "alt") {
+	    key["alt"] = true;
+    } else if (key["current"] == "meta") {
+	    key["meta"] = true;
+    } else if (key["current"] == " ") {
+	    key["space"] = true;
     }
 
-  });
+  //   if(key['control'] && key['shift'] || key["current"] == "f12") {
+  // } else {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  // }
 
-  window.alert = alert = parent.notification;
-
-
-
-
-  function spawnContextMenu(content) {
-    if(document.getElementsByClassName("contextMenu")[0]) {
-      document.getElementsByClassName("contextMenu")[0].outerHTML = "";
+    if(key['control'] == true) {
+      
     }
-    var newelement = document.createElement("div"); 
-    newelement.classList.add("contextMenu");
-    newelement.style.left = data.system.mouse.x + "px";
-    newelement.style.top = data.system.mouse.y + "px";
-    document.body.append(newelement);
 
-    for(var i = 0; content.length > i; i++) {
-      newelement = document.getElementsByClassName("contextMenu")[0];
-      if(content[i][0] == "<hr>") {
-        var newcontextelement = document.createElement("hr"); 
-      } else {
-        var newcontextelement = document.createElement("a"); 
-        newcontextelement.innerHTML = content[i][0];
-        newcontextelement.href = "javascript:"+content[i][1];
-        if(content[i][2] && content[i][2] == "disabled") {
-          newcontextelement.removeAttribute("href")
+    if(key['shift'] && key['space']) {
+      if(key['current'] == "w") {
+        if(!ismainos) {
+          parent.unrun(data.mypid);
         }
-  
       }
-      newelement.append(newcontextelement);
+      
     }
 
-    newelement.parentElement.addEventListener("click",function() {
-      newelement.outerHTML = "";
-    })
-  }
+    if(key['alt'] == true) {
+    }
+
+    if(key['meta'] == true) {
+    }
+	})
+
+	window.addEventListener("keyup", function (event) {
+	  key["current"] = event.key.toLowerCase();
+    if (key["current"] == "control") {
+	    key["control"] = false;
+	  } else if (key["current"] == "shift") {
+	    key["shift"] = false;
+    } else if (key["current"] == "alt") {
+	    key["alt"] = false;
+    } else if (key["current"] == "meta") {
+	    key["meta"] = false;
+    } else if (key["current"] == " ") {
+	    key["space"] = false;
+    }
+
+	})
