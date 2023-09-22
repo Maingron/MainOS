508e8479d80d00ef3743dfb0404e927adde722a5
Maingron <r@maingron.com>
2023-09-22 21:56:40 +0200

    Add file attributes to file explorer

---
+++ Program Files/Explorer/inner/modules/file_attributes.js
@@ -0,0 +1,50 @@
+function initFileAttributes() {
+	attachedModules.push(applyFileAttributesToAll);
+}
+
+function applyFileAttributesToAll() {
+	for(fileElement of document.getElementById("content_files").children) {
+		// if file name starts with ., add class hidden
+		if(fileElement.innerText.startsWith(".")) {
+			fileElement.classList.add("hidden");
+		}
+		var fileAttributes = getAttributes(fileElement.getAttribute("path"));
+		// find entry starting with A=
+		for(attribute of fileAttributes) {
+			if(attribute.startsWith("A=")) {
+				attribute = attribute.split("A=")[1];
+				attribute = attribute.split("");
+				
+				var newColorDotDiv = document.createElement("div");
+				newColorDotDiv.classList.add("color_dot_div");
+				fileElement.appendChild(newColorDotDiv);
+
+				for(a_attribute of attribute) {
+					if(["A", "B", "C", "D", "E", "F", "0"].includes(a_attribute)) {
+						addColorDot(newColorDotDiv, a_attribute);
+
+						if(a_attribute == "0") {
+							fileElement.classList.add("system_file");
+						}
+					} else if(["a"].includes(a_attribute)) {
+						fileElement.classList.add("hidden");
+					} else if(["!"].includes(a_attribute)) {
+						fileElement.classList.add("system_file");
+						fileElement.classList.add("outdated_file");
+						addColorDot(newColorDotDiv, "outdated");
+					}
+				}
+			}
+		}
+	}
+}
+
+initFileAttributes();
+
+
+function addColorDot(colorDotDiv, color) {
+	var newColorDot = document.createElement("div");
+	newColorDot.classList.add("color_dot");
+	newColorDot.classList.add("color_dot_" + color);
+	colorDotDiv.appendChild(newColorDot);
+}
