25e3bbddf377062f053e5e2c5226e02c08a8400d
Maingron <r@maingron.com>
2024-06-09 02:48:09 +0200

    Make Settings Reset Button work again; Hide some settings

--- Program Files/settings/scripts.js
+++ Program Files/settings/scripts.js
@@ -38,9 +38,8 @@ var settings = [
     {category: "Advanced Settings", id: "advanced", settings: [
         {name: "Developer Mode", type: "input>checkbox", id: "system.user.settings.developer.enable"},
         {name: "Enable Online Repository", type: "input>checkbox", id: "system.user.settings.enableRepository"},
-        {name: `Reset ${system.osDetails.name}`, type: "button", id: "dummy.resetos", onclick: "os.formatfs('yes')"},
-        {name: `Reset ${system.osDetails.name} and freeze`, type: "button", id: "dummy.resetosandfreeze", onclick: "os.formatfs('yes', false)"},
-        {name: "Update through Downgrade", type: "button", id: "dummy.updatethroughdowngrade", onclick: "", disabled: true}
+        {name: `Reset ${system.osDetails.name}`, type: "button", id: "dummy.resetos", onclick: "parent.localStorage.clear(); top.window.location.reload();"},
+        // {name: "Update through Downgrade", type: "button", id: "dummy.updatethroughdowngrade", onclick: "", disabled: true}
     ]},
     {category: "User settings", id: "user", settings: [
         {name: "Your Username", type: "input>text", id: "system.user.name", disabled: true},
