3880c8ef8777958498b72583537200a98edd6b3a
Maingron <r@maingron.com>
2024-12-21 15:21:20 +0100

    Improve run window

--- Program Files/run/script.js
+++ Program Files/run/script.js
@@ -1,8 +1,33 @@
-function submit(inputcontent) {
-	parent.run(inputcontent);
-	pWindow.focus();
+function submit(inputcontent = document.querySelector("#input").value) {
+	if(system.user.programs[inputcontent]) {
+		try {
+			parent.run(inputcontent);
+			pWindow.close();
+		} catch(e) {
+			pWindow.os.sendNotification({
+				"title": "Error in Run",
+				"content": "An error occurred while trying to run " + document.querySelector("#input").value,
+				"type": "error",
+				"sender": this,
+				"isAlert": true
+			});
+		}
+
+	} else {
+		pWindow.os.sendNotification({
+			"title": "Error in Run",
+			"content": "Could not find a program to run for query \"" + document.querySelector("#input").value + "\"",
+			"type": "error",
+			"sender": this,
+			"isAlert": true
+		});
+	}
 }
 
 pWindow.setMaximized(false);
 parent.resizeWindow(pWindow.getWindow(), 360, 220);
 pWindow.focus();
+
+window.setTimeout(function() {
+	document.querySelector("#input").focus();
+}, 10);
