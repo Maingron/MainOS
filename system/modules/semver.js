bc16611e957ed02f7d4d2c2cc370c44e97fb6993
Maingron <r@maingron.com>
2025-02-14 20:30:51 +0100

    Use getters and setters in semver version() This temporarily removes the build-part

--- system/modules/semver.js
+++ system/modules/semver.js
@@ -1,65 +1,59 @@
 export function version(versionString = 0) {
 	// https://semver.org/
 
-	if(typeof versionString === 'number') {
-		versionString = "" + versionString;
-	} else if(typeof versionString == 'object' && versionString.toString) {
-		versionString = versionString.toString();
-	} else if(typeof versionString !== 'string') {
-		throw new Error("Got incompatible version format");
-	}
-
-	let version = versionString?.split('.');
-
-	let major = version[0] || 0;
-	let minor = version[1] || 0;
-	let patch = version[2] || 0;
-	let build = version[3] || undefined;
-
-	// Assign patch if patch is given
-	if(patch && patch?.includes("-")) {
-		build = patch.split(patch.split("-")[0] + "-")[1];
-		patch = patch.split("-")[0];
-	}
-
-	// Clean output
-	if(major) {
-		major = major.replace(/\D/g, '');
-	}
-	if(minor) {
-		minor.replace(/\D/g, '');
-	}
-	if(patch) {
-		patch.replace(/\D/g, '');
-	}
-
-	if(!build) {
-		build = undefined;
-	}
+	let major, minor, patch;
 
 	let toString = function() {
 		let outputString = `${major}.${minor}.${patch}`;
-		if(build) {
-			outputString += `.${build}`;
-		}
 		return outputString;
 	}
 
 	const returnObj = {
-		major: +major,
-		minor: +minor,
-		patch: +patch,
-		build: +build || build,
 		toString: toString,
 		isNewerThan: function(right, verbose = false) {
 			if(!right) {
 				throw new Error("Expected a version to compare to.");
 			}
 			return isNewerThan(this, right, verbose);
+		},
+		get major() {
+			return major;
+		},
+		set major(value) {
+			major = (("" + value).replace(/\D/g, '') || 0);
+		},
+		get minor() {
+			return minor;
+		},
+		set minor(value) {
+			minor = (("" + value).replace(/\D/g, '') || 0);
+		},
+		get patch() {
+			return patch;
+		},
+		set patch(value) {
+			patch = (("" + value).replace(/\D/g, '') || 0);
+		},
+		get version() {
+			return toString();
+		},
+		set version(value) {
+			if(typeof value !== 'string' && typeof value !== 'number' && typeof value !== 'object') {
+				throw new Error("Got incompatible version format (expected string, number or version object).");
+			}
+
+			if(typeof value == 'object' && value.toString) {
+				value = value.toString();
+			}
+
+			value = ("" + value).split('.');
+			this.major = value[0] || 0;
+			this.minor = value[1] || 0;
+			this.patch = value[2] || 0;
 		}
 	};
 
-	Object.freeze(returnObj);
+	returnObj.version = versionString;
 
 	return returnObj;
 }
