27fda05e718dc95520b68ce53b51bd4b4d86c57f
Maingron <r@maingron.com>
2025-12-16 22:38:06 +0100

    Ask if user wants to save changes in dirty notepad

--- Program Files/notepad/notepad.js
+++ Program Files/notepad/notepad.js
@@ -9,6 +9,7 @@ if (path) {
     object.textcontent.value = iofs.load(path);
     object.savebutton.innerHTML = "Save as " + path;
     object.filename1.style.display = "none";
+	object.filename1.value = path;
 
 	addEventListener("message", () => {
 		if(event.data == "pWindowReady") {
@@ -26,6 +27,7 @@ function savetextfile() {
     } else {
         iofs.save(filename1.value, object.textcontent.value, "t=txt", 1);
     }
+	textcontent.removeAttribute("changed");
 }
 
 setInterval(function () {
@@ -43,3 +45,91 @@ if (objects.textcontent.value.includes("font=")) {
 if (objects.textcontent.value.includes("fontsize=")) {
     objects.textcontent.style.fontSize = objects.textcontent.value.split("fontsize=")[1].split(";")[0];
 }
+
+textcontent.addEventListener("change", () => {
+	textcontent.setAttribute("changed", "changed");
+});
+
+addEventListener("message", () => {
+	if(event.data == "pWindowReady") {
+		pWindow.onBeforeUnrun = function() {
+			if(!textcontent.getAttribute("changed")) {
+				return;
+			}
+
+			pWindow.interactionLock = os.popupWindow.generatePopupWindow({
+				"preset": "buttonAlert3",
+				"title": "Save?",
+				"text": `
+					Do you want to save changes to this document before closing?
+				`,
+				"sender": this,
+				"actionButtons": [
+					{
+						"label": "Save",
+						"action": "saveAction",
+						"closePopup": true,
+						"autofocus": true
+					},
+					{
+						"label": "Don't Save",
+						"action": "dontSaveAction",
+						"closePopup": true
+					},
+					{
+						"label": "Cancel",
+						"action": "cancelAction",
+						"closePopup": true
+					}
+				],
+				"actions": {
+					"saveAction": function() {
+						if(!object.filename1.value || object.filename1.value.trim() == "") {
+							os.popupWindow.generatePopupWindow({
+								"preset": "buttonAlert3",
+								"title": "Error",
+								"text": "Please enter a valid filename first.",
+								"type": "error",
+								"actionButtons": [
+									{
+										"label": "OK",
+										"action": "cancelAction",
+										"closePopup": true,
+										"autofocus": true
+									}
+								],
+								"actions": {
+									"cancelAction": function() {
+										pWindow.interactionLock = false;
+										object.filename1.focus();
+									}
+								},
+							});
+							return false;
+						} else {
+							pWindow.interactionLock = false;
+							savetextfile();
+							pWindow.close();
+						}
+					},
+					"dontSaveAction": function() {
+						pWindow.interactionLock = false;
+						pWindow.close(true);
+					},
+					"cancelAction": function() {
+						pWindow.interactionLock = false;
+					}
+				},
+				"actionInputs": [
+					{
+						"type": "text",
+						"id": "filenameinput",
+						"placeholder": "Filename"
+					}
+				]
+				});
+
+			return false;
+		}
+	}
+});
