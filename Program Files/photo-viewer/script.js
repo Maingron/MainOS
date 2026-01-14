49dc6f2026ddc4a32e8ec95a26e5ebd56060cb8a
Maingron <r@maingron.com>
2026-01-14 03:34:40 +0100

    Make photo viewer work with async file loading

--- Program Files/photo-viewer/script.js
+++ Program Files/photo-viewer/script.js
@@ -1,19 +1,26 @@
 var path = window.parent.attr;
 
-console.log(window.parent.attr);
+window.addEventListener("message", () => {
+	if(event.data == "pWindowReady") {
+		if (path) {
+			document.querySelector("#photo").classList.add("loading");
 
+			iofs.loadPromise(path, false).then(resultImage => {
+				document.querySelector("#photo").classList.remove("loading");
+				document.querySelector("#photo").src = resultImage;
+				document.title += " - " + iofs.getInfos(path).name;
+				
+			});
 
-if (path) {
-    document.querySelector("#photo").src = "#iofs:" + path;
-    document.title += " - " + iofs.getInfos(path).name;
+			if(iofs.getInfos(path).mime.category != "image") {
+				document.querySelector("#errortext").innerHTML = "<p>(This doesn't seem like an image)</p>";
+			} else {
+				document.querySelector("#errortext").outerHTML = "";
+			}
 
-    if(iofs.getInfos(path).mime.category != "image") {
-        document.querySelector("#errortext").innerHTML = "<p>(This doesn't seem like an image)</p>";
-    } else {
-        document.querySelector("#errortext").outerHTML = "";
-    }
-
-} else {
-    document.querySelector("#photo").style.display = "none";
-    document.querySelector("#errortext").innerHTML += "<p>... It seems like you didn't open a photo.</p>";
-}
+		} else {
+			document.querySelector("#photo").style.display = "none";
+			document.querySelector("#errortext").innerHTML += "<p>... It seems like you didn't open a photo.</p>";
+		}
+	}
+});
